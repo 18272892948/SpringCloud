31026bb13ec5253b3bdd8923378428c7886953ce
root <1197919258@qq.com>
2019-06-03 10:41:43 +0800

    11111

---
+++ LooginConsumer/src/main/java/com/xwt/login/LoginApplicationWeb.java
@@ -0,0 +1,21 @@
+package com.xwt.login;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableSwagger2
+public class LoginApplicationWeb {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LoginApplicationWeb.class,args);
+
+    }
+}
